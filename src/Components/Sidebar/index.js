ebaf3ae490eabe1190ef4da72bbcf2532c093c09
Wesley L. Handy <wesleylhandy@gmail.com>
2019-11-03 16:42:30 -0500

    add special styling for empty notes

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -3,29 +3,28 @@ import checkMark from "../../SVG/check-mark.svg";
 import classNames from "classnames";
 import "./sidebar.css";
 
-const getNotesRows = (notes, currentNoteIndex, selectNote) => {
-	// TODO fix the selected row highlight, which breaks on subsequent clicks to the sidebar.
-	return notes.map(({ subject, read }, idx) => (
-		<div
-			key={`note-${idx}`}
-			className={classNames("NotesSidebarItem", {
-				selected: idx === currentNoteIndex,
-			})}
-			onClick={e => selectNote(idx)}
-			id={`note-${idx}`}
-			role="button"
-		>
-			<h4 className="NotesSidebarItem__title">{subject}</h4>
-			{read && <img alt="Check Mark" src={checkMark} />}
-		</div>
-	));
-};
-
 const Sidebar = ({ notes = [], currentNoteIndex, selectNote }) => (
 	<section className="NotesSidebar">
 		<h2 className="NotesSidebar__title">Available Notes:</h2>
 		<div className="NotesSidebar__list">
-			{notes.length && getNotesRows(notes, currentNoteIndex, selectNote)}
+			{notes.length ? (
+				notes.map(({ subject, read }, idx) => (
+					<div
+						key={`note-${idx}`}
+						className={classNames("NotesSidebarItem", {
+							selected: idx === currentNoteIndex,
+						})}
+						onClick={e => selectNote(idx)}
+						id={`note-${idx}`}
+						role="button"
+					>
+						<h4 className="NotesSidebarItem__title">{subject}</h4>
+						{read && <img alt="Check Mark" src={checkMark} />}
+					</div>
+				))
+			) : (
+				<div className="NotesSidebarItem empty">--Empty--</div>
+			)}
 		</div>
 	</section>
 );
